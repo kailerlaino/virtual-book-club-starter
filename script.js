fd0c601a6024dc96c001cb88c12d5a3d3a6b170f
Kailer Laino <kailer@meta.com>
2025-06-04 16:35:00 -0700

    finish handleReviewSubmit

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function loadReviews() {
   console.log("loadReviews called!");
-  const reviewList = document.getElementById('reviews-list');
+  const reviewList = document.getElementById("reviews-list");
 
   for (const review of reviews) {
-    const reviewElement = createReviewElement(review);
-    reviewList.appendChild(reviewElement);
+    createReviewElement(review);
   }
 }
 
@@ -16,26 +15,39 @@ function repostReview() {
   console.log("Repost button clicked");
 }
 
-const handleReviewSubmit = () => {
-  console.log("handleReviewSubmit called!");
+const handleReviewSubmit = (event) => {
+  event.preventDefault();
+  const title = document.getElementById("book-title").value;
+  const review = document.getElementById("review-text").value;
+  const rating = document.getElementById("rating").value;
+
+  const newReview = {
+    id: reviews.length + 1,
+    title,
+    reviewText: review,
+    rating,
+    likes: 0,
+    reposts: 0,
+  };
+  createReviewElement(newReview);
 };
 
 function createReviewElement(review) {
   console.log(review);
 
-const container = document.getElementById("reviews-list");
+  const container = document.getElementById("reviews-list");
 
   const reviewElementDiv = document.createElement("div");
-  reviewElementDiv.setClass = ("review-item");
+  reviewElementDiv.setClass = "review-item";
 
   reviewElementDiv.innerHTML = `
     <h3>${review.title}</h3>
     <p>${review.reviewText}</p>
-    <p>${review.reviewRating}</p>
-    <button id="like-${review.id} onclick="toggleLike()" class=>Like</button>
-    <button id="repost-${review.id} onclick="repostReview()">Repost</button>
+    <p>${review.rating}</p>
+    <button id="like-${review.id} onclick="toggleLike()" class=>Like (${review.likes})</button>
+    <button id="repost-${review.id} onclick="repostReview()">Repost (${review.reposts})</button>
     `;
-    return reviewElementDiv;
+  container.appendChild(reviewElementDiv);
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -45,7 +57,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const form = document.getElementById("review-form");
 
   form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    handleReviewSubmit();
+    handleReviewSubmit(event);
   });
 });
